897aeccf5b55f9898354ade0c418a16f29669601
Swati Sonalika Bihari <134080886+SwatiSonalikaBihari@users.noreply.github.com>
2025-12-17 23:22:28 +0530

    Update script.py

--- script/script.py
+++ script/script.py
@@ -1,10 +1,12 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_unixtime, to_timestamp
 spark = SparkSession.builder.getOrCreate()
 database_name = "default"
 table_name = "ingest_year_2025"
 df = spark.read.table(f"{database_name}.{table_name}")
 df.show(truncate=False)
+
+
+from pyspark.sql.functions import col, from_unixtime, to_timestamp
 
 df_out = df.withColumn(
     "event_time",
@@ -12,7 +14,7 @@ df_out = df.withColumn(
 ).show()
 
 
-s3_output_path = "s3://akhil-iceberg/transformed_data-new/cicd"
+s3_output_path = "s3://akhil-iceberg/transformed_data-new/"
 df.write \
     .format("JSON") \
     .mode("overwrite").save(s3_output_path)
